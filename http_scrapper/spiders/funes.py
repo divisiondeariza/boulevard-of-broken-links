0595aaa26e026be6ca15aa46af33c12935b15974
Emmanuel Ariza <emmanuel@emmanuel.emmanuel>
2017-07-14 00:26:18 -0500

    Now url is gor from console

--- http_scrapper/spiders/funes.py
+++ http_scrapper/spiders/funes.py
@@ -3,6 +3,7 @@ import scrapy
 from scrapy.spiders.crawl import CrawlSpider, Rule
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 from http_scrapper.items import HttpScrapperItem
+from scrapy.http.request import Request
 
 def formatLink(link):
     if link[-1] == "/":
@@ -11,11 +12,13 @@ def formatLink(link):
 
 class FunesSpider(CrawlSpider):
     name = 'funes'
-    domain = 'funes.uniandes.edu.co'
-    allowed_domains = [domain]
-    start_urls = ['http://' + domain + '/']
-
-    rules = [Rule(LxmlLinkExtractor(allow=(), allow_domains = domain, process_value = formatLink), 'parse_items', follow =True)]
+#     domain = 'funes.uniandes.edu.co'
+#     allowed_domains = [domain]
+#     start_urls = ['http://' + domain + '/']
+    rules = [Rule(LxmlLinkExtractor(allow=(),  process_value = formatLink), 'parse_items', follow =True)]
+    
+    def start_requests(self):
+        yield Request(url=self.user_input)
     
     def parse_items(self, response):
         item = HttpScrapperItem()
